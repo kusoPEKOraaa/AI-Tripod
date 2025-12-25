15b8acd512351b4121ea4391bf3961b43e52975b
kusoPEKOraaa <37388583@qq.com>
2025-12-26 04:14:34 +0800

    重置

--- ai_tripod_backend/api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class APIConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_tripod_backend.api'
--- ai_tripod_backend/webui/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebUIConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_tripod_backend.webui'
